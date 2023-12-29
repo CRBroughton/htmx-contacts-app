4851cadcc141f574a1f931a6fe4be7c8cd894e78
CRBroughton <crbroughton@posteo.uk>
2023-12-29 18:20:02 +0000

    feat: :sparkles: successful tailwind integration

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -2,7 +2,7 @@ import { Vector } from '../index'
 
 export default function test({ vectors }: { vectors: Vector[] }) {
   return (
-    <div>
+    <div class="bg-red-300">
       <h1>About me!</h1>
       {vectors.map((vector) => <p>{vector.x} - {vector.y}</p>)}
     </div>
--- src/hono.tsx
+++ src/hono.tsx
@@ -1,13 +1,20 @@
-import { Hono } from 'hono'
+import { Hono  } from 'hono'
+import { serveStatic } from 'hono/bun'
 import { jsxRenderer } from 'hono/jsx-renderer'
 
 const hono = new Hono()
 
+hono.use('/styles.css', serveStatic({ path: './styles.css' }))
 hono.get(
   '*',
   jsxRenderer(({ children }) => {
     return (
       <html>
+        <head>
+          {/* <script src="https://unpkg.com/htmx.org@1.9.3"/> */}
+          <link rel="stylesheet" href="styles.css"/>
+          <title>Hono + htmx</title>
+        </head>
         <body>
           <div>{children}</div>
         </body>
