33b4991748ccff77eb44d7c1f4faf2728dccaf81
CRBroughton <crbroughton@posteo.uk>
2023-12-29 19:24:45 +0000

    refactor: :recycle: move route to demo file

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,5 @@
 import hono from './hono'
-import Test from './components/test'
 import demo from './routes/demo'
-
-const numbers = [ 1, 2, 3 ]
-
-hono.get('/', (c) => {
-  return c.render(
-    <div>
-      <Test numbers={numbers} />
-    </div>
-  )
-})
 
 hono.route('/', demo)
 
--- src/routes/demo.tsx
+++ src/routes/demo.tsx
@@ -1,7 +1,15 @@
 import { Hono } from 'hono'
 import CustomButton from '../components/button'
+import Test from '../components/test'
 
 const demo = new Hono()
+
+const numbers = [ 1, 2, 3 ]
+
+demo.get('/', (c) => {
+  return c.render(<div><Test numbers={numbers} /></div>
+  )
+})
 
 demo.get('/id/:id', (c) => {
   return c.render(<CustomButton num={5}/>)
