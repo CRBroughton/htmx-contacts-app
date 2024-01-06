746c981de3b23390db8701cc6ff2b8f817f67519
CRBroughton <crbroughton@posteo.uk>
2024-01-06 09:54:48 +0000

    feat: :sparkles: add infinite scroll

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -2,13 +2,14 @@ export default function PaginationButton({ page }: { page:number }){
   return (
     <tr>
       <td colspan={5} style="text-align: center">
-        <button
+        <span
           hx-target="closest tr"
           hx-swap="outerHTML"
           hx-select="tbody > tr"
-          hx-get={`/contacts?page=${page+1}`}>
-                Load More
-        </button>
+          hx-get={`/contacts?page=${page+1}`}
+          hx-trigger="revealed">
+                Loading More
+        </span>
       </td>
     </tr>
   )
