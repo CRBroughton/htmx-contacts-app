08cd1a217c6c6365a10a1cbbc622194aa87abb6f
CRBroughton <crbroughton@posteo.uk>
2024-06-02 11:06:24 +0100

    feat: :construction: create migrations for sessions and users

---
+++ src/migrations/create_session_table.ts
@@ -0,0 +1,15 @@
+import { Kysely } from 'kysely'
+import { KyselyDatabase } from '@/schema'
+
+export async function up(db: Kysely<KyselyDatabase>): Promise<void> {
+  await db.schema
+    .createTable('session')
+    .addColumn('id', 'integer', (col) => col.primaryKey().autoIncrement())
+    .addColumn('user_id', 'varchar')
+    .addColumn('expires_at', 'date')
+    .execute()
+}
+
+export async function down(db: Kysely<KyselyDatabase>): Promise<void> {
+  await db.schema.dropTable('sessin').execute()
+}
---
+++ src/migrations/create_user_table.ts
@@ -0,0 +1,14 @@
+import { Kysely } from 'kysely'
+import { KyselyDatabase } from '@/schema'
+
+export async function up(db: Kysely<KyselyDatabase>): Promise<void> {
+  await db.schema
+    .createTable('user')
+    .addColumn('id', 'integer', (col) => col.primaryKey().autoIncrement())
+    .addColumn('username', 'varchar')
+    .execute()
+}
+
+export async function down(db: Kysely<KyselyDatabase>): Promise<void> {
+  await db.schema.dropTable('users').execute()
+}
