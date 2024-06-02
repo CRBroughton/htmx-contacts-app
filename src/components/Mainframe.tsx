ecf183d80a8debf45f5a110e258ff66504baabd2
CRBroughton <crbroughton@posteo.uk>
2024-06-02 20:07:36 +0100

    feat: :sparkles: add custom pico styling - makes things smaller

--- src/components/Mainframe.tsx
+++ src/components/Mainframe.tsx
@@ -6,11 +6,11 @@ export default function Mainframe({ children }: {children: Child}) {
       <head>
         <script src="public/htmx-1.9.10.min.js"/>
         <link rel="stylesheet" href="/styles.css"/>
-        <link rel="stylesheet" href="/custom.css"/>
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.slate.min.css"
         />
+        <link rel="stylesheet" href="/custom.css"/>
         <script src="./ws.js"></script>
         <title>Hono + htmx</title>
       </head>
